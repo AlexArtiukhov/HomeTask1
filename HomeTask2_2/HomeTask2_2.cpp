df661f3615e03604926448a992f62094a0457838
AlexArtiukhov <alex.artikov@gmail.com>
2021-10-09 09:47:48 +0300

    add 3 hometasks

---
+++ HomeTask2_2/HomeTask2_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int number;
+
+int main()
+{
+    cout << "Enter number: ";
+    cin >> number;
+    while (number > 0) {
+        int digit = number % 10;
+        number = number / 10;
+        cout << digit;
+    }
+}
---
+++ HomeTask2_3/HomeTask2_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x1;
+    int x2;
+    cin >> x1;
+    cin >> x2;
+    int y1;
+    int y2;
+    cin >> y1;
+    cin >> y2;
+    cout << sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}

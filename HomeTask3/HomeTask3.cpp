91b5404fe805adca234bdf9925b12b05a722c3a5
AlexArtiukhov <alex.artikov@gmail.com>
2021-10-15 22:55:15 +0300

    add 3 HW

---
+++ HomeTask3/HomeTask3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 0, c = 0, d = 0;
+
+    cin >> a >> b >> c;
+
+    d = (b * b) + (-4 * a * c);
+
+    cout <<
+        (-b + sqrt(d)) / (2*a) << endl << (-b - sqrt(d)) / (2*a) << endl;
+}

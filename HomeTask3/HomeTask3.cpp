a4dceb0528363c0931c652be8194aa52f0ddb285
AlexArtiukhov <alex.artikov@gmail.com>
2021-10-20 17:41:02 +0300

    fixed discrimminante

--- HomeTask3/HomeTask3.cpp
+++ HomeTask3/HomeTask3.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -8,6 +9,13 @@ int main()
 	cin >> a >> b >> c;
 
 	int d = (b * b) + (-4 * a * c);
-	cout << (-b + sqrt(d)) / (2 * a) << endl
-		<< (-b - sqrt(d)) / (2 * a) << endl;
+	if (d < 0)
+	{
+		cout << "d is < than 0";
+	}
+	else
+	{
+		cout << (-b + sqrt(d)) / (2 * a) << endl
+			<< (-b - sqrt(d)) / (2 * a) << endl;
+	}
 }

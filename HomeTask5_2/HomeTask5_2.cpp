7dc985e195d42d64d393dd3ffdb3e76c930f6579
AlexArtiukhov <alex.artikov@gmail.com>
2021-11-06 08:10:13 +0200

    add hw5_1, hw5_2

---
+++ HomeTask5_2/HomeTask5_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double rslt = sqrt(5);
+
+	for (int i = 10; i < 95; i += 5)
+	{
+		rslt = rslt * sqrt(i);
+	}
+
+	cout << rslt;
+}
---
+++ HomeWork_5_1/HomeWork_5_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int summ = 0;
+
+	for (int i = 10; i < 100; i++)
+	{
+		summ += i;
+	}
+
+	cout << summ;
+}

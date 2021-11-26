05cd5594ced46d8567fb309dffb89ef4b778b1be
AlexArtiukhov <alex.artikov@gmail.com>
2021-11-26 23:39:12 +0200

    add hw89

---
+++ HomeTask8_1/HomeTask8_1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Year = 0;
+
+	cin >> Year;
+
+	for (int i = ++Year; true; i++)
+	{
+		int a = i % 10;
+		int b = (i / 10) % 10;
+		int c = (i / 100) % 10;
+		int d = (i / 1000) % 10;
+
+		bool IsUnic1digit = (a != b && a != c && a != d);
+		bool IsUnic2digit = (b != c && b != d);
+		bool IsUnic3digit = (c != d);
+
+		if (IsUnic1digit && IsUnic2digit && IsUnic3digit)
+		{
+			cout << i << endl;
+			break;
+		}
+	}
+}
---
+++ HomeTask8_2/HomeTask8_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	string mantions;
+	int sum = 0;
+
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> mantions;
+		
+		if (mantions[0] + mantions[2] + mantions[4] > 1)
+		{
+			sum += 1;
+		}
+	}
+	cout << mantions;
+}

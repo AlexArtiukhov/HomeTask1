e370b9cb488a2b77ffe7c0fecd819ad357c9aff8
AlexArtiukhov <alex.artikov@gmail.com>
2021-12-03 23:16:44 +0200

    add 9 hw

---
+++ HomeTask9_1/HomeTask9_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string  Words;
+    string BiggestWord;
+    string CurrentWord;
+    int BiggestWordLenth = 0;
+    int WordLenth = 0;
+
+    getline(cin, Words);
+
+        for (int i = 0; i < Words.length(); i++)
+        {
+            if (Words[i] == ',' || Words[i] == ' ' || Words[i] == '.' || Words[i] == '!' || Words[i] == '?' || Words[i] == '-')
+            {
+                if (WordLenth > BiggestWordLenth)
+                {
+                    BiggestWord = CurrentWord;
+                    BiggestWordLenth = WordLenth;
+                }
+
+                CurrentWord = "";
+                WordLenth = 0;
+            }
+            else
+            {
+                CurrentWord += Words[i];
+                WordLenth++;
+            }
+        }
+
+    cout << BiggestWord;
+}
---
+++ HomeTask9_2/HomeTask9_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    string  Word;
+    string BiggestWord;
+    string CurrentWord;
+    int BiggestWordLenth = 0;
+    int WordLenth = 0;
+
+    getline(cin, Word);
+
+    for (int i = 0; i < Word.length(); i++)
+    {
+        if (Word[i] == ',' || Word[i] == ' ' || Word[i] == '.' || Word[i] == '!' || Word[i] == '?' || Word[i] == '-')
+        {
+            if (WordLenth > BiggestWordLenth)
+            {
+                BiggestWord = CurrentWord;
+                BiggestWordLenth = WordLenth;
+            }
+
+            CurrentWord = "";
+            WordLenth = 0;
+        }
+        else if (i < Word.length())
+        {
+            CurrentWord += Word[i];
+            WordLenth++;
+        }
+    }
+
+    cout << BiggestWordLenth;
+}

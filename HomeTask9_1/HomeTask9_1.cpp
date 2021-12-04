bd4345f83b329c2550a39935edb93b8c678c083a
AlexArtiukhov <alex.artikov@gmail.com>
2021-12-04 13:16:31 +0300

    fix 9 hw

--- HomeTask9_1/HomeTask9_1.cpp
+++ HomeTask9_1/HomeTask9_1.cpp
@@ -5,33 +5,32 @@ using namespace std;
 
 int main()
 {
-    string  Words;
-    string BiggestWord;
-    string CurrentWord;
-    int BiggestWordLenth = 0;
-    int WordLenth = 0;
+	string Words;
+	string BiggestWord;
+	string CurrentWord;
+	int BiggestWordLenth = 0;
+	int WordLenth = 0;
 
-    getline(cin, Words);
+	getline(cin, Words);
 
-        for (int i = 0; i < Words.length(); i++)
-        {
-            if (Words[i] == ',' || Words[i] == ' ' || Words[i] == '.' || Words[i] == '!' || Words[i] == '?' || Words[i] == '-')
-            {
-                if (WordLenth > BiggestWordLenth)
-                {
-                    BiggestWord = CurrentWord;
-                    BiggestWordLenth = WordLenth;
-                }
+	for (int i = 0; i < Words.length(); i++)
+	{
+		if (Words[i] == ',' || Words[i] == ' ' || Words[i] == '.' || Words[i] == '!' || Words[i] == '?' || Words[i] == '-')
+		{
+			if (WordLenth > BiggestWordLenth)
+			{
+				BiggestWord = CurrentWord;
+				BiggestWordLenth = WordLenth;
+			}
 
-                CurrentWord = "";
-                WordLenth = 0;
-            }
-            else
-            {
-                CurrentWord += Words[i];
-                WordLenth++;
-            }
-        }
-
-    cout << BiggestWord;
+			CurrentWord = "";
+			WordLenth = 0;
+		}
+		else
+		{
+			CurrentWord += Words[i];
+			WordLenth++;
+		}
+	}
+	cout << BiggestWord;
 }
--- HomeTask9_2/HomeTask9_2.cpp
+++ HomeTask9_2/HomeTask9_2.cpp
@@ -5,35 +5,39 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+	string Words;
+	string BiggestWord;
+	string CurrentWord;
+	int BiggestWordLenth = 0;
+	int WordLenth = 0;
+	bool isUnic = true;
 
-    string  Word;
-    string BiggestWord;
-    string CurrentWord;
-    int BiggestWordLenth = 0;
-    int WordLenth = 0;
+	getline(cin, Words);
 
-    getline(cin, Word);
+	for (int i = 0; i < Words.length(); i++)
+	{
+		if (Words[i] == ',' || Words[i] == ' ' || Words[i] == '.' || Words[i] == '!' || Words[i] == '?' || Words[i] == '-')
+		{
+			if (WordLenth > BiggestWordLenth && isUnic)
+			{
+				BiggestWord = CurrentWord;
+				BiggestWordLenth = WordLenth;
+			}
 
-    for (int i = 0; i < Word.length(); i++)
-    {
-        if (Word[i] == ',' || Word[i] == ' ' || Word[i] == '.' || Word[i] == '!' || Word[i] == '?' || Word[i] == '-')
-        {
-            if (WordLenth > BiggestWordLenth)
-            {
-                BiggestWord = CurrentWord;
-                BiggestWordLenth = WordLenth;
-            }
+			CurrentWord = "";
+			WordLenth = 0;
+			isUnic = true;
+		}
+		else
+		{
+			CurrentWord += Words[i];
+			WordLenth++;
+		}
 
-            CurrentWord = "";
-            WordLenth = 0;
-        }
-        else if (i < Word.length())
-        {
-            CurrentWord += Word[i];
-            WordLenth++;
-        }
-    }
-
-    cout << BiggestWordLenth;
+		if (Words[i] == Words[i+1])
+		{
+			isUnic = false;
+		}
+	}
+	cout << BiggestWordLenth;
 }

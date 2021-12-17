f68bcc3ac224402a0c99082ff1b7b4dc412ba60a
AlexArtiukhov <alex.artikov@gmail.com>
2021-12-18 00:18:57 +0200

    add 10 hw

---
+++ HomeTask10_2/HomeTask10_2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void find(int n, int div)
+{
+    if (n == 1)
+    {
+        return;
+    }
+
+    if (n % div == 0)
+    {
+        cout << " " << div;
+        find(n / div, div);
+    }
+    else
+    {
+        if (div == 2)
+        {
+            find(n, div + 1);
+        }
+        else
+        {
+            find(n, div + 2);
+        }
+    }
+}
+int main()
+{
+    int n, div = 2;
+    cin >> n;
+    cout << n;
+    find(n, 2);
+    cin.get(); cin.get();
+    return 0;
+}

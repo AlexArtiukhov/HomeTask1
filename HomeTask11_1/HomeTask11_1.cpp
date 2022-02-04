21cc27382debbd1b0fb93e01620747d7030f5d43
AlexArtiukhov <alex.artikov@gmail.com>
2022-02-04 23:34:08 +0200

    add 11 hw

---
+++ HomeTask11_1/HomeTask11_1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int elementsAmount = 1000;
+	int arr[elementsAmount];
+	int arr2[elementsAmount];
+	int Colnum = 0;
+	int n;
+	bool isUnic = true;
+
+	cin >> n;
+	cout << endl;
+
+	srand(time(0));
+
+	for (int i = 0; i < elementsAmount; i++)
+	{
+		arr[i] = rand() % n;
+	}
+
+	for (int i2 = 0; i2 < elementsAmount; i2++)
+	{
+		cout << arr[i2] << endl;
+	}
+
+	cout << endl;
+
+	for (int j = 0; j < elementsAmount; j++)
+	{
+		for (int y = j + 1; y < elementsAmount; y++)
+		{
+			if (arr[j] == arr[y])
+			{
+				Colnum++;
+				arr2[j] = arr[j];
+			}
+		}
+
+		for (int p = 0; p < j; p++)
+		{
+			if (arr[j] == arr2[p])
+			{
+				isUnic = false;
+			}
+		}
+
+		if (Colnum != 0 && isUnic)
+		{
+			cout << arr[j] << " - " << Colnum + 1 << endl;
+		}
+		Colnum = 0;
+		isUnic = true;
+	}
+}
